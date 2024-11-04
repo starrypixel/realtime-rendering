a26b87345987dd61e19861d221fa7d2fe3f9b59c
th-y-tra-n <augusttran@outlook.com>
2024-11-04 14:28:25 -0500

    Add vec3 class

---
+++ vec3/vec3.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Thy Tran on 10/3/24.
+//
+
+#include "vec3.h"
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2){
+    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+inline vec3 operator-(const vec3 &v1, const vec3 &v2){
+    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v1, const vec3 &v2){
+    return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+
+inline vec3 operator/(const vec3 &v1, const vec3 &v2){
+    return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2){
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+}
+
+inline vec3 cross(const vec3 &v1, const vec3 &v2){
+    return vec3 ( v1.e[1] * v2.e[2] - v1.e[2]*v2.e[1],
+                (- (v1.e[0] * v2.e[2] - v1.e[2]*v2.e[0])),
+                (v1.e[0]*v2.e[1] - v1.e[1] * v2.e[0])
+    );
+}
+
+inline vec3 unit_vector(vec3 v){
+    return v / v.length();
+}
